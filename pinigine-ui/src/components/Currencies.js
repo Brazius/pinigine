a261e8fdfdef31fc192cb3b74443d34c468ea9e3
Tomas Brazys <brazius@gmail.com>
2020-07-20 16:50:26 +0300

    add delete, update method

--- pinigine-ui/src/components/Currencies.js
+++ pinigine-ui/src/components/Currencies.js
@@ -10,15 +10,60 @@ class Currencies extends Component {
     }
 
     componentDidMount() {
-        fetch('http://localhost:8080/currencies', { headers: { "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU5NTMzMDkzNn0.3TwAdUoeglA6PjliXEbRtV_pSWo91U5b_jCJE48XT-3VIFLt2eutoiHrrRWdR-Yt77fxb6cv5mPQJe_ydc5qmw" } })
+        fetch('http://localhost:8080/currencies',
+            {
+                headers: {
+                    "Authorization":
+                        "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU5NTMzMDkzNn0.3TwAdUoeglA6PjliXEbRtV_pSWo91U5b_jCJE48XT-3VIFLt2eutoiHrrRWdR-Yt77fxb6cv5mPQJe_ydc5qmw",
+                    "Content-Type": "application/json"
+                }
+            })
             .then((response) => response.json())
             .then((responseData) => {
                 this.setState({
                     currencies: responseData
                 })
             })
-            .catch(err => console.error(err))
+            .catch(err => console.error("error: " + err));
     }
+
+    onDelClick = (id) => {
+        if (window.confirm('Are you sure to delete currency?')) {
+            fetch('http://localhost:8080/currencies/' + id, {
+                method: 'DELETE',
+                headers: new Headers({
+                    "Authorization":
+                        "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU5NTMzMDkzNn0.3TwAdUoeglA6PjliXEbRtV_pSWo91U5b_jCJE48XT-3VIFLt2eutoiHrrRWdR-Yt77fxb6cv5mPQJe_ydc5qmw"
+                })
+            }).then(res => this.fetchCurrencies())
+                .catch(err => console.error(err));
+        }
+    };
+
+    updateCurrency(currency) {    
+        fetch('http://localhost:8080/currencies', {    
+            method: 'PUT',    
+            headers: {    
+                "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU5NTMzMDkzNn0.3TwAdUoeglA6PjliXEbRtV_pSWo91U5b_jCJE48XT-3VIFLt2eutoiHrrRWdR-Yt77fxb6cv5mPQJe_ydc5qmw",    
+                "Content-Type": "application/json"    
+      },    
+            body: JSON.stringify(currency)    
+        })    
+            .then(res => this.fetchCurrencies())    
+            .catch(err => console.log(err))    
+    }  
+
+    editable = (cell) => {
+        return (
+            <div style={ {backgroundColor: "#fafafa"} } contentEditable suppressContentEditableWarning onBlur={e => {
+                const curr = [...this.state.currencies];
+                curr[cell.index][cell.column.id] = e.target.innerHTML;
+                this.setState({currencies: curr});
+            }}
+                 dangerouslySetInnerHTML={ {__html: this.state.currencies[cell.index][cell.column.id]} }
+            />
+        );
+    };
 
     render() {
         const columns = [{
@@ -35,6 +80,24 @@ class Currencies extends Component {
         }, {
             Header: 'Rate',
             accessor: 'rate'
+        }, {
+            sortable: false,
+            filterable: false,
+            width: 100,
+            Cell: row => (
+                <div>
+                    <button onClick={() => this.onDelClick(row.original.id)}>Delete</button>
+                </div>
+            )
+        }, {
+            sortable: false,
+            filterable: false,
+            width: 100,
+            Cell: row => (
+                <div>
+                    <button onClick={() => this.updateCurrency(row.original.id)}>Update</button>
+                </div>
+            )
         }
             ,];
         return (
