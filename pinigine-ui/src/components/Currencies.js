4be9ca023de9c0b05acf4ecbe25850069967691e
Tomas Brazys <brazius@gmail.com>
2020-07-22 14:29:51 +0300

    add login, update currensies.js to ui

--- pinigine-ui/src/components/Currencies.js
+++ pinigine-ui/src/components/Currencies.js
@@ -6,66 +6,70 @@ import AddCurrency from './AddCurrency';
 class Currencies extends Component {
     constructor(props) {
         super(props);
-        this.state = { currencies: [] };
+        this.state = {currencies: []};
     }
 
     componentDidMount() {
-        fetch('http://localhost:8080/currencies',
-            {
-                headers: {
-                    "Authorization":
-                        "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU5NTMzMDkzNn0.3TwAdUoeglA6PjliXEbRtV_pSWo91U5b_jCJE48XT-3VIFLt2eutoiHrrRWdR-Yt77fxb6cv5mPQJe_ydc5qmw",
-                    "Content-Type": "application/json"
-                }
-            })
-            .then((response) => response.json())
-            .then((responseData) => {
-                this.setState({
-                    currencies: responseData
-                })
-            })
-            .catch(err => console.error("error: " + err));
+        this.fetchCurrencies();
     }
 
     onDelClick = (id) => {
+        const jwtToken = sessionStorage.getItem("jwt");
         if (window.confirm('Are you sure to delete currency?')) {
             fetch('http://localhost:8080/currencies/' + id, {
                 method: 'DELETE',
                 headers: new Headers({
-                    "Authorization":
-                        "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU5NTMzMDkzNn0.3TwAdUoeglA6PjliXEbRtV_pSWo91U5b_jCJE48XT-3VIFLt2eutoiHrrRWdR-Yt77fxb6cv5mPQJe_ydc5qmw"
+                    "Authorization": jwtToken,
+                    "Content-Type": "application/json"
                 })
             }).then(res => this.fetchCurrencies())
                 .catch(err => console.error(err));
         }
     };
 
-    updateCurrency(currency) {    
-        fetch('http://localhost:8080/currencies', {    
-            method: 'PUT',    
-            headers: {    
-                "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU5NTMzMDkzNn0.3TwAdUoeglA6PjliXEbRtV_pSWo91U5b_jCJE48XT-3VIFLt2eutoiHrrRWdR-Yt77fxb6cv5mPQJe_ydc5qmw",    
-                "Content-Type": "application/json"    
-      },    
-            body: JSON.stringify(currency)    
-        })    
-            .then(res => this.fetchCurrencies())    
-            .catch(err => console.log(err))    
-    }  
+    addCurrency(currency) {
+        const jwtToken = sessionStorage.getItem("jwt");
+        fetch('http://localhost:8080/currencies/', {
+            method: 'POST',
+            headers: {
+                "Authorization": jwtToken,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(currency)
+        })
+            .then(res => this.fetchCurrencies())
+            .catch(err => console.log(err))
+    }
+
+    updateCurrency(currency) {
+        const jwtToken = sessionStorage.getItem("jwt");
+        fetch('http://localhost:8080/currencies', {
+            method: 'PUT',
+            headers: {
+                "Authorization": jwtToken,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(currency)
+        })
+            .then(res => this.fetchCurrencies())
+            .catch(err => console.log(err))
+    }
 
     editable = (cell) => {
         return (
-            <div style={ {backgroundColor: "#fafafa"} } contentEditable suppressContentEditableWarning onBlur={e => {
+            <div style={{backgroundColor: "#ffffff"}} contentEditable suppressContentEditableWarning onBlur={e => {
                 const curr = [...this.state.currencies];
                 curr[cell.index][cell.column.id] = e.target.innerHTML;
                 this.setState({currencies: curr});
             }}
-                 dangerouslySetInnerHTML={ {__html: this.state.currencies[cell.index][cell.column.id]} }
+                 dangerouslySetInnerHTML={{__html: this.state.currencies[cell.index][cell.column.id]}}
             />
         );
     };
 
+
     render() {
+
         const columns = [{
             Header: 'Currency',
             accessor: 'name',
@@ -95,18 +99,34 @@ class Currencies extends Component {
             width: 100,
             Cell: row => (
                 <div>
-                    <button onClick={() => this.updateCurrency(row.original.id)}>Update</button>
+                    <button onClick={() => this.updateCurrency(row.original)}>Save</button>
                 </div>
             )
         }
+
             ,];
+
         return (
-            <div className="App">
-                <AddCurrency addCurrency={this.addCurrency} fetchCurrencies={this.fetchCurrencies} />
-                <ReactTable data={this.state.currencies} columns={columns} filterable={true} />
+            <div>
+                <AddCurrency addCurrency={this.addCurrency} fetchCurrencies={this.fetchCurrencies}/>
+                <ReactTable data={this.state.currencies} columns={columns} filterable={true}/>
             </div>
         );
     }
+
+    fetchCurrencies = () => {
+        const jwtToken = sessionStorage.getItem("jwt");
+        fetch('http://localhost:8080/currencies',
+            {headers: {"Authorization": jwtToken, "Content-Type": "application/json"}}
+        )
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    currencies: responseData
+                })
+            })
+            .catch(err => console.error("error: " + err));
+    }
 }
 
-export default Currencies;  
+export default Currencies;
